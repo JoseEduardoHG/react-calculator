526dd4409b0e52cd426cecb2a848a87675167bef
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-12 23:35:56 -0500

    fix: styles for double width button and change type of width to boolean

--- src/components/CalculatorButton.tsx
+++ src/components/CalculatorButton.tsx
@@ -1,12 +1,11 @@
 import classNames from 'classnames';
 
 type SymbolType = 'number' | 'operator' | 'equal' | 'clear';
-type Width = 1 | 2;
 
 type CalculatorButtonProps = {
   children?: React.ReactNode;
   label?: string;
-  width?: Width;
+  isDoubleWidth?: boolean;
   symbol?: SymbolType;
   onClick?: () => void;
 };
@@ -15,22 +14,20 @@ export default function CalculatorButton({
   children,
   label,
   onClick,
-  width = 1,
   symbol = 'number',
+  isDoubleWidth = false,
 }: CalculatorButtonProps) {
   return (
     <button
-      className={classNames(
-        'p-4 text-2xl font-bold text-white rounded-full shadow-md',
-        {
-          'bg-gray-200': symbol === 'number',
-          'bg-yellow-400': symbol === 'operator',
-          'bg-green-500': symbol === 'equal',
-          'bg-red-500': symbol === 'clear',
-          'grow-0': width === 1,
-          grow: width === 2,
-        },
-      )}
+      className={classNames({
+        'basis-1/4 aspect-square shadow-md rounded-lg font-bold text-2xl text-white':
+          true,
+        'bg-slate-500': symbol === 'number',
+        'bg-yellow-400': symbol === 'operator',
+        'bg-red-500': symbol === 'clear',
+        'bg-blue-500': symbol === 'equal',
+        'basis-2/4 aspect-[2/1]': isDoubleWidth,
+      })}
       onClick={onClick}
     >
       {children ?? label ?? ''}
