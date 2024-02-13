aaf7063b2530e981c1aa85bc52aef07425d5e00e
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-12 23:35:21 -0500

    feat: add styles for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Calculator from '@/components/Calculator';
 
 export default function Home() {
   return (
-    <div>
+    <section className='container bg-slate-100 flex justify-center items-center min-h-screen w-full p-4'>
       <Calculator />
-    </div>
+    </section>
   );
 }
